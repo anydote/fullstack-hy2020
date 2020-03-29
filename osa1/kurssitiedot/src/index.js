f8420282fecbb2008d6634940b0f9d0f5bc8f562
Antti <antti.maatta@gmail.com>
2020-03-29 12:53:18 +0300

    Poista tarpeettomat div-elementit komponenteista

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -2,37 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
+  return <h1>{props.course}</h1>
 }
 
 const Part = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.count}</p>
-    </div>
-  )
+  return <p>{props.name} {props.count}</p>
 }
 
 const Content = (props) => {
   return (
-    <div>
+    <>
       <Part name={props.part1} count={props.exercises1} />
       <Part name={props.part2} count={props.exercises2} />
       <Part name={props.part3} count={props.exercises3} />
-    </div>
+    </>
   )
 }
 
 const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.total}</p>
-    </div>
-  )
+  return <p>Number of exercises {props.total}</p>
 }
 
 const App = () => {
