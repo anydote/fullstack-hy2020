1e8e468eed9c73beb1962fc48370252c4a6f8768
Antti <antti.maatta@gmail.com>
2020-03-29 12:28:12 +0300

    Refaktoroi Total-komponentti osan 1.1 mukaisesti

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -25,6 +25,14 @@ const Content = (props) => {
   )
 }
 
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -40,7 +48,7 @@ const App = () => {
       <Content part1={part1} exercises1={exercises1} 
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
