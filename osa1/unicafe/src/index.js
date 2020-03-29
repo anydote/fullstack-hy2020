ae158f3efa2726cb4b6ae7d298837af92b6834c7
Antti <antti.maatta@gmail.com>
2020-03-29 18:29:27 +0300

    Lisää tehtävän 1.6 painikkeet

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({ text }) => <h1>{text}</h1>
+const Display = ({ counter }) => <div>{counter}</div>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setNeutral(neutral + 1)
+
   return (
     <div>
-      code here
+      <Header text="give feedback" />
+      <Button 
+        handleClick={increaseGood}
+        text="good"
+      />
+      <Button 
+        handleClick={increaseNeutral}
+        text="neutral"
+      />
+      <Button 
+        handleClick={increaseBad}
+        text="bad"
+      />
     </div>
   )
 }
