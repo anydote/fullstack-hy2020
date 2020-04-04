54cfa95450edf48c993eaa27015d200cf28dc300
Antti <antti.maatta@gmail.com>
2020-04-04 18:02:16 +0300

    Estä saman nimen lisäys uudelleen (2.7)

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -14,9 +14,15 @@ const App = () => {
       name: newName,
       id: persons.length + 1,
     }
-  
-    setPersons(persons.concat(personObject))
-    setNewName('')
+
+    let names = persons.map(p => p.name)
+    
+    if (names.includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
   }
 
   const handleNameChange = (event) => {
