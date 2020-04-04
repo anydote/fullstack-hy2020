f6b0750dbb9835697bb23ec0b665616b8980d8b7
Antti <antti.maatta@gmail.com>
2020-04-04 17:51:08 +0300

    Lisää nimet näyttävä puhelinluettelo tehtävän 2.6 mukaisesti

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,27 +1,44 @@
 import React, { useState } from 'react'
 
+const Person = ({ person }) => <div>{person.name}</div>
+
+const Phonebook = ({ persons }) => persons.map((person) => <Person person={person} />)
+
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <form onSubmit={addPerson}>
+        name: 
+        <input 
+          value={newName} 
+          onChange={handleNameChange}
+        />
+        <p><button type="submit">add</button></p>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Phonebook persons={persons} />
     </div>
   )
-
 }
 
 export default App
