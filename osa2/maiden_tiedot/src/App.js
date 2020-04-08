e77f8f2f798e7d32635118a5cfe59fbd602d3bac
Antti <antti.maatta@gmail.com>
2020-04-08 20:38:18 +0300

    Lisää maan nimen viereen nappi (2.13)

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,58 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-
-const Filter = ({ filter, handleFilterChange }) => {
-  return (
-    <>
-      <form>
-          find countries
-          <input 
-            value={filter} 
-            onChange={handleFilterChange}
-          />
-      </form>
-    </>
-  )
-}
-
-const CountryDisplay = ({ countries, filter }) => {
-  let filteredCountries = countries.filter(
-    country => country.name.toLowerCase().includes(filter.toLowerCase())
-  )
-  
-  if (filteredCountries.length > 10) {
-    return "Too many matches, specify another filter"
-  } else if (filteredCountries.length > 1 && filteredCountries.length <= 10) {
-    return <CountryList countries={filteredCountries} />
-  } else if (filteredCountries.length === 1) {
-    return <SingleCountryDisplay country={filteredCountries[0]} />
-  } else {
-    return ""
-  }
-}
-
-const CountryList = ({ countries }) => countries.map((c) => <div>{c.name}</div>)
-
-const SingleCountryDisplay = ({ country }) => (
-  <>
-    <h1>{country.name}</h1>
-    <div>capital {country.capital}</div>
-    <div>population {country.population}</div>
-    <h2>Languages</h2>
-    <ul>
-      {country.languages.map(language => 
-        <li key={language.iso639_1}>
-          {language.name}
-        </li>
-      )}
-    </ul>
-    <img src={country.flag} width="200"/>
-  </>
-)
+import Filter from './components/Filter'
+import CountryDisplay from './components/CountryDisplay'
 
 const App = () => {
   const [ countries, setCountries] = useState([])
   const [ filter, setFilter ] = useState('')
+  const [ chosenCountry, setChosenCountry ] = useState('')
 
   useEffect(() => {
     console.log('effect')
@@ -67,12 +21,22 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
+    setChosenCountry('')
+  }
+
+  const handleCountryClick = (event) => {
+    setChosenCountry(event.target.dataset.country)
   }
 
   return (
     <div>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      <CountryDisplay countries={countries} filter={filter} />
+      <CountryDisplay 
+        countries={countries} 
+        filter={filter} 
+        chosenCountry={chosenCountry}
+        handleCountryClick={handleCountryClick}
+      />
     </div>
   );
 }
---
+++ osa2/maiden_tiedot/src/components/CountryDisplay.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const CountryDisplay = ({
+	filter,
+	countries,
+	chosenCountry,
+	handleCountryClick
+}) => {
+	const filteredCountries = countries.filter(
+		country => country.name.toLowerCase().includes(filter.toLowerCase())
+	)
+
+	if (chosenCountry !== "") {
+		const theCountry = countries.find(c => c.name === chosenCountry)
+		return <SingleCountryDisplay country={theCountry} />
+	}
+
+	if (filteredCountries.length > 10) {
+		return "Too many matches, specify another filter"
+	} else if (filteredCountries.length > 1 && filteredCountries.length <= 10) {
+		return <CountryList countries={filteredCountries} handleCountryClick={handleCountryClick} />
+	} else if (filteredCountries.length === 1) {
+		return <SingleCountryDisplay country={filteredCountries[0]} />
+	} else {
+		return ""
+	}
+}
+
+const CountryList = ({ countries, handleCountryClick }) => {
+	return (
+		countries.map((c) => 
+			<Country
+				key={c.name}
+				name={c.name} 
+				handleCountryClick={handleCountryClick} 
+			/>
+		)
+	)
+}
+
+const Country = ({ name, handleCountryClick }) => {
+	return (
+		<div>
+			{name}
+			<button 
+				key={name}
+				data-country={name} 
+				onClick={handleCountryClick}>
+				show
+			</button>
+		</div>
+	)
+}
+
+const SingleCountryDisplay = ({ country }) => (
+	<>
+		<h1>{country.name}</h1>
+		<div>capital {country.capital}</div>
+		<div>population {country.population}</div>
+		<h2>Languages</h2>
+		<ul>
+			{country.languages.map(language =>
+				<li key={language.iso639_1}>
+					{language.name}
+				</li>
+			)}
+		</ul>
+		<img src={country.flag} alt="" width="200" />
+	</>
+)
+
+export default CountryDisplay;
